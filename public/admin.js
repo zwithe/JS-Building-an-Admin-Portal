163defc89493642be150f88fb054bd6459161ada
Joshua Miles <joshua.m@hackerusa.com>
2021-05-28 11:27:29 -0500

    finished solution code

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,43 @@
 
 // Your Code Here
+
+async function main() {
+
+    let response = await fetch('http://localhost:9001/getBooks')
+
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+function renderBook(book) {
+    let root = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:9001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
+}
+
+main();
